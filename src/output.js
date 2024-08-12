73cd43fbdf249aac62192a92f4ff6e01518b848e
V_Champion <bannalana@inbox.lv>
2024-08-12 17:35:27 +0100

    Update output.js

--- src/output.js
+++ src/output.js
@@ -7,25 +7,37 @@ const parse = require('./parse');
 let scriptLocationMap = new Map();
 let outputChannel;
 
+function getLinkForOutput(scriptLocationIndex, scriptLocation, linkData, document) {
+    const {filePath, lineNumber} = linkData;
+
+    const start = document.positionAt(scriptLocationIndex);
+    const end = document.positionAt(scriptLocationIndex + scriptLocation.length);
+
+    const uri = vscode.Uri.parse(`command:nexus-sync.openFile?${encodeURIComponent(JSON.stringify([filePath, lineNumber]))}`);
+
+    const link = new vscode.DocumentLink(new vscode.Range(start, end), uri);
+    link.tooltip = "Open Script in Editor";
+
+    return link;
+}
+
 async function documentLinks(document) {
     const text = document.getText();
     const links = [];
 
-    // Sort the entries by scriptLocation length in descending order
+    let startIndex = 0;
+
     const sortedEntries = Array.from(scriptLocationMap.entries()).sort((a, b) => 
         parse.extractScriptLocation(b[0], b[1].messageType).length - parse.extractScriptLocation(a[0], a[1].messageType).length
     );
     
     for (const [message, linkData] of sortedEntries) {
-        const {filePath, lineNumber, messageType} = linkData;
-        const scriptLocation = parse.extractScriptLocation(message, messageType);
-
-        let startIndex = 0;
+        const scriptLocation = parse.extractScriptLocation(message, linkData.messageType);
 
         if (!scriptLocation) {
             continue;
         }
-
+        
         while (startIndex !== -1) {
             startIndex = text.indexOf(message, startIndex);
 
@@ -33,15 +45,7 @@ async function documentLinks(document) {
                 const scriptLocationIndex = text.indexOf(scriptLocation, startIndex);
 
                 if (scriptLocationIndex !== -1 && scriptLocationIndex < startIndex + message.length) {
-                    const start = document.positionAt(scriptLocationIndex);
-                    const end = document.positionAt(scriptLocationIndex + scriptLocation.length);
-
-                    const uri = vscode.Uri.parse(`command:nexus-sync.openFile?${encodeURIComponent(JSON.stringify([filePath, lineNumber]))}`);
-
-                    const link = new vscode.DocumentLink(new vscode.Range(start, end), uri);
-                    link.tooltip = "Open Script in Editor";
-
-                    links.push(link);
+                    links.push(getLinkForOutput(scriptLocationIndex, scriptLocation, linkData, document));
                 }
                 startIndex += message.length;
             }
